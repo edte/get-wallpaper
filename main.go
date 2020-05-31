874c95711fe799687d8ae9bd2ca2994df6d3b585
edte <zzzzip6@gmail.com>
2020-05-31 20:23:05 +0800

    feat: add some files

--- main.go
+++ main.go
@@ -1,33 +1,120 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"regexp"
+	"strconv"
+	"time"
 )
-sudo
-func main() {
-	url := "https://ss.netnr.com/wallpaper"
-	resp, err := http.Get(url)
-	if err != nil {
-		errors.New("get url failed!")
-	}
 
-	body, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(body))
+const (
+	basePageUrl = "https://wall.alphacoders.com/by_resolution.php?w=1920&h=1080&lang=Chinese&page="
+	numsReg     = "<title>(.*?)\\s1920x1080\\s高清壁纸"
+	PagesReg    = "桌面背景\\sID:(.*?)\""
+	perPageReg  = "width=\"1920\" height=\"1080\" src=\"(.*?)\""
+)
+
+var (
+	PageNum int
+	ids     []string
+	urls    []string
+	imgUlrs []string
+)
+
+type Url struct {
+	url     string
+	baseUrl string
+	pageNum int
 }
 
-func m() {
-	url := "http://p1.qhimg.com/bdm/1366_768_85/t0119918a57c366a5c5.jpg"
-	resp, err := http.Get(url)
-	if err != nil {
-		errors.New("get url faild!")
-	}
+func (u *Url) Url() string {
+	return u.url
+}
 
-	data, err := ioutil.ReadAll(resp.Body)
+func (u *Url) SetPageNum(pageNum int) {
+	u.pageNum = pageNum
+}
+
+func NewUrl(page int) *Url {
+	return &Url{url: basePageUrl + strconv.Itoa(page)}
+}
+
+func (u *Url) GetBody() (string, error) {
+	resp, err := http.Get(u.Url())
 	if err != nil {
-		errors.New("save jpg faild!")
+		return "", err
 	}
-	ioutil.WriteFile("/home/edte/pictures/"+"a.jpg", data, 0644)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
+
+func GetPagesNum() {
+	body, _ := NewUrl(1).GetBody()
+	num := regexp.MustCompile(numsReg)
+	strs := num.FindAllStringSubmatch(body, -1)[0][1]
+	PageNum, _ = strconv.Atoi(strs)
+}
+
+func GetPagesUrl() {
+	body, _ := NewUrl(1).GetBody()
+	pages := regexp.MustCompile(PagesReg)
+	t := pages.FindAllStringSubmatch(body, -1)
+	for _, v := range t {
+		ids = append(ids, v[1])
+	}
+}
+
+func GetUrl(id string) string {
+	return "https://wall.alphacoders.com/big.php?i=" + id + "&lang=Chinese"
+}
+
+func GetUrls() {
+	for _, v := range ids {
+		urls = append(urls, GetUrl(v))
+	}
+}
+
+func GetImgUrls() {
+	for _, url := range urls {
+		fmt.Println(url)
+		reps, _ := http.Get(url)
+		defer reps.Body.Close()
+		body, _ := ioutil.ReadAll(reps.Body)
+		page := regexp.MustCompile(perPageReg)
+		s := page.FindAllStringSubmatch(string(body), -1)
+		imgUlrs = append(imgUlrs, s[0][1])
+	}
+	fmt.Println(imgUlrs)
+}
+
+func SaveImgByUrl() {
+	for _, v := range imgUlrs {
+		fmt.Println(v)
+		resp, _ := http.Get(v)
+		defer resp.Body.Close()
+		data, err := ioutil.ReadAll(resp.Body)
+		fmt.Println(err)
+		ioutil.WriteFile("/home/edte/pictures/"+GetTimeUnix()+".jpg", data, 0644)
+	}
+}
+
+func main() {
+	GetPagesUrl()
+	fmt.Println("#")
+	GetUrls()
+	fmt.Println("##")
+	GetImgUrls()
+	fmt.Println("###")
+	SaveImgByUrl()
+}
+
+func GetTimeUnix() string {
+	t := time.Now().Unix()
+	return strconv.FormatInt(t, 10)
 }
